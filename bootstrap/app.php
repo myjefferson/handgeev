d2856418e019bff28b5e2d212ea378bd468e2081
Jefferson Carvalho <jcsjeffrey@gmail.com>
2025-10-23 09:55:45 -0300

    delete trustProxies because security

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -31,9 +31,7 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
-        $middleware->trustProxies(at: '*');
-        
+    ->withMiddleware(function (Middleware $middleware) {        
         $middleware->api(prepend: [
             \Illuminate\Http\Middleware\HandleCors::class,
         ]);
