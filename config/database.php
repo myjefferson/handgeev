411bd37fdfcdc165951544f24b4290a7314884ba
Jefferson Carvalho <jcsjeffrey@gmail.com>
2025-10-24 14:56:03 -0300

    change configurations from redis

--- config/database.php
+++ config/database.php
@@ -144,11 +144,6 @@ return [
     'redis' => [
         'client' => env('REDIS_CLIENT', 'phpredis'),
 
-        'options' => [
-            'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
-        ],
-
         'default' => [
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
@@ -156,13 +151,6 @@ return [
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', 6379),
             'database' => env('REDIS_DB', 0),
-        ],
-
-        'queue' => [
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', 6379),
-            'database' => env('REDIS_CACHE_DB', 1),
         ],
 
         'cache' => [
