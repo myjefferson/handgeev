063df1072ead575075011c877f384d57be44ba73
Jefferson Carvalho <jcsjeffrey@gmail.com>
2026-01-08 01:46:18 -0300

    fix app.blade.php from Vite manifest

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -30,7 +30,6 @@
         @routes
         @viteReactRefresh
         @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
-        @vite(['resources/css/app.css'])
         @inertiaHead
     </head>
     <body class="font-sans antialiased">
